09f79e8c0cea881d5901daad109140a56aa0c246
Rilton Franzone <riltonfranzone@gmail.com>
2023-04-10 11:19:06 -0300

    strcat implementation

---
+++ strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* concatenate t to the end of s */
+void strcat(char *s, char *t) {
+	while(*s != '\0') s++;
+	while(*t != '\0') *s++ = *t++;
+}
+
+int main() {
+	char str[80] = "initial";
+  strcat(str, " test");
+	puts(str);
+}
